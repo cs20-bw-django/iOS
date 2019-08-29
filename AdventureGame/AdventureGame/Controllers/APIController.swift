4f1d345f85f5ae8d3f316a7660ad434f34c9c4d7
Angel <angelbuenrostro@outlook.com>
2019-08-29 05:15:25 -0400

    Add Login/Signup view controller

---
+++ AdventureGame/AdventureGame/Controllers/APIController.swift
@@ -0,0 +1,19 @@
+//
+//  APIController.swift
+//  AdventureGame
+//
+//  Created by Angel Buenrostro on 8/29/19.
+//  Copyright © 2019 Angel Buenrostro. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class APIController {
+    
+    private let baseURL = URL(string: "")!
+
+    var bearer: Bearer?
+    
+    
+}
